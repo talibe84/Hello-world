7e6a3e9607e2f0c730e15b967c46d8e6105c14bc
talibe84 <43723341+talibe84@users.noreply.github.com>
2019-03-08 00:54:34 -0500

    Add files via upload
    Monte Carlo Simmulation Model  -

---
+++ Random1.h
@@ -0,0 +1,16 @@
+//
+//  Random1.h
+//  Hello Quant_World
+//
+//  Created by Mamoudou Diallo on 3/6/19.
+//  Copyright © 2019 Talibé. All rights reserved.
+//
+
+#ifndef Random1_h
+#define Random1_h
+
+double GetOneGaussianBySummation();
+double GetOneGaussianByBoxMuller();
+
+
+#endif /* Random1_h */
